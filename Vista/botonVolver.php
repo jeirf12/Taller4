405bb3b4e60da27a49930006af430c75a88bdb10
jeirf12 <jeirf12@github.com>
2022-02-11 23:07:37 -0500

    added button back

---
+++ Vista/botonVolver.php
@@ -0,0 +1,5 @@
+<?php if(isset($isForm) && !$isForm): ?>
+<button class="button-back button-danger" type="submit" action="?s=Sesion&a=volver">Regresar</button>
+<?php else: ?>
+<button class="button-danger" type="submit" action="?s=Sesion&a=Volver">Regresar</button>
+<?php endif; ?>
