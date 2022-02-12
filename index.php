dd2dc31343355dc6a9000d88f3a0862cf083c872
adrianfgp <55796422+adrianfgp@users.noreply.github.com>
2022-02-12 18:33:03 -0500

    0.43

--- index.php
+++ index.php
@@ -6,13 +6,13 @@ if(isset($_REQUEST['c'])){
     $controlador = $_REQUEST['c'];
     $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'iniciarSesion';
     
-    require_once "controlador/controlador.$controlador";
+    require_once "Controlador/controlador.$controlador";
     $controlador = "controlador".$controlador;
     $controlador = new $controlador;
- 
+
 }
 else{
-    require_once "controlador/controlador.$controlador";
+    require_once "Controlador/controlador.$controlador";
     $controlador = "controlador".$controlador;
     $controlador = new $controlador;
     $accion = 'Listar'; 
