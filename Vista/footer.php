999e257da850d5302b4ce19708db8b2f37128f01
adrianfgp <adrianf@unicauca.edu.co>
2022-03-13 22:45:05 -0500

    v11.3

--- Vista/footer.php
+++ Vista/footer.php
@@ -4,6 +4,6 @@
       <a title="Mira nuestro perfil de facebook" target="_blank" href="https://www.facebook.com/Apimacizo/" class="fa-brands fa-facebook"></a>
       <a title="Mira nuestro perfil de youtube" target="_blank" href="https://www.youtube.com/channel/UCYJ-q3AHS5JdojiIhCh6hZQ" class="fa-brands fa-youtube"></a>
     </div>
-    <p>© copyright 2022 &nbsp; | &nbsp; Desarrollado por Adrian &nbsp; Jhonny &nbsp; Jhonfer</p>
+    <p>© Copyright 2022. Todos los derechos reservados. &nbsp; | &nbsp; Desarrollado por Adrian &nbsp; Jhonny &nbsp; Jhonfer</p>
   </div>
 </footer>
--- Vista/inicio.php
+++ Vista/inicio.php
@@ -4,7 +4,7 @@
   <div class="container">
     <div class="body-container" style="color: white">
       <h1>Bienvenido</h1>
-      <p></p>
+      <p>Esta es la pagina web de Apimacizo, aquí podrás realizar tus pedidos a la empresa desde la comodidad de tu casa. Contamos con una gran variedad de productos consumibles, además de la herramienta necesaria para los apicultores.</p>
       <div class="video"><iframe class="video-content" width="500" height="300" src="https://www.youtube.com/embed/Y6fDKg8rAJA" title="Apimacizo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
     </div>
   </div>
