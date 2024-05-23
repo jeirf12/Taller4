78013e123feb31a5ef71428f8461edba03d7db6d
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2024-05-23 16:25:36 -0500

    fix problem with date year current

--- Vista/componentes/navegacion/footer.php
+++ Vista/componentes/navegacion/footer.php
@@ -4,6 +4,6 @@
       <a title="Mira nuestro perfil de Facebook" target="_blank" href="https://www.facebook.com/Apimacizo/" class="fa-brands fa-facebook"></a>
       <a title="Mira nuestro perfil de Youtube" target="_blank" href="https://www.youtube.com/channel/UCYJ-q3AHS5JdojiIhCh6hZQ" class="fa-brands fa-youtube"></a>
     </div>
-    <p>© Copyright 2022. Todos los derechos reservados. &nbsp; | &nbsp; Desarrollado por Adrian &nbsp; Jhonny &nbsp; Jhonfer</p>
+    <p>© Copyright <?php echo date('Y'); ?>. Todos los derechos reservados. &nbsp; | &nbsp; Desarrollado por Adrian &nbsp; Jhonny &nbsp; Jhonfer</p>
   </div>
 </footer>
