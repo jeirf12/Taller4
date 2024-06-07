6c9eb161476088dba987137eb6a76055d55c9084
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2024-06-07 11:45:34 -0500

    added new logo and modify icon pages

--- Vista/componentes/navegacion/head.php
+++ Vista/componentes/navegacion/head.php
@@ -1,6 +1,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="Vista/css/logo2.png">
   <link rel="stylesheet" type="text/css" href="Vista/css/style.css">
   <link rel="stylesheet" type="text/css" href="Vista/css/card.css">
   <link rel="stylesheet" type="text/css" href="Vista/css/popup.css">
